a508b206a61041ae6f40267f8f1044a6324dd12e
GinMu <gin.musheng@gmail.com>
2025-06-05 12:01:07 +0800

    display export dag duration

--- dag.js
+++ dag.js
@@ -11,13 +11,16 @@ export default class Dag {
   async exportDag(cid) {
     const dist = path.join("car", `${cid.toString()}.car`);
     const stream = fs.createWriteStream(dist);
-
+    const start = Date.now();
     for await (const buf of this.car.stream(cid)) {
       console.log(`buffer size: ${buf.length} bytes`);
       stream.write(buf);
     }
+    const end = Date.now();
 
     stream.end();
-    console.log(`DAG exported to ${dist}`);
+    const duration = (end - start) / 1000;
+
+    console.log(`DAG exported to ${dist}`, `, time: ${duration.toFixed(2)} seconds`);
   }
 }
